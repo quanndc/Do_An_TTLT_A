1e151587c0e7f0cd08dd285a15e31f5c369ae906
PhamIchChuyen220603 <phamichchuyen@gmail.com>
2023-10-10 09:18:31 +0700

    first commit from chuyen

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>...</Text>
+      <Text>Ích Chuyên đẹp trai</Text>
       <StatusBar style="auto" />
     </View>
   );
